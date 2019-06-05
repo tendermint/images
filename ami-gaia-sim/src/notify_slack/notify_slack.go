5cb201e20cfb6e51cca87b70f4bad8a0688a1daf
mircea-c <mircea@tendermint.com>
2019-06-04 20:46:01 -0400

    new path for gaia sim app

--- ami-gaia-sim/src/notify_slack/notify_slack.go
+++ ami-gaia-sim/src/notify_slack/notify_slack.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-
 	message_ts := flag.String("ts", "", "Message timestamp")
 	channel_id := flag.String("channel-id", "", "Channel ID")
 	seed_failed := flag.Bool("failed", false, "Failed seed flag")
@@ -23,14 +22,12 @@ func main() {
 	var message string
 	if *seed_failed {
 		message = "Simulation with *seed " + *seed_num + "* failed! To replicate, run the command:  ```" +
-			`go test -mod=readonly ./cmd/gaia/app -run TestFullGaiaSimulation \
-								-SimulationEnabled=true \
-								-SimulationNumBlocks=` + *num_blocks + ` \
-								-SimulationVerbose=true \
-								-SimulationCommit=true \
-								-SimulationSeed=` + *seed_num + ` \
-								-SimulationPeriod=` + *period + ` \
-								-v -timeout 24h` + "```"
+			`go test github.com/cosmos/cosmos-sdk/simapp -run TestFullAppSimulation \
+					-SimulationEnabled=true -SimulationNumBlocks=` + *num_blocks + ` \
+					-SimulationVerbose=true -SimulationCommit=true \
+					-SimulationSeed=` + *seed_num + ` \
+					-SimulationPeriod=` + *period + ` \
+					-v -timeout 24h` + "```"
 
 	} else {
 		message = "Host finished running seeds: " + *seeds
@@ -48,8 +45,7 @@ func main() {
 	} else if *log_file_path != "" {
 		log_file_params := slack.FileUploadParameters{
 			Title: "Simulation log",
-			File: *log_file_path,
-
+			File:  *log_file_path,
 		}
 		_, err := slack_api.UploadFile(log_file_params)
 		if err != nil {
@@ -62,13 +58,4 @@ func main() {
 			return
 		}
 	}
-}
-
-func send_message (slack_api *slack.Client, channel_id string, message string) string {
-
-	_, ts, err := slack_api.PostMessage(channel_id, slack.MsgOptionText(message, false))
-	if err != nil {
-		fmt.Printf("%s\n", err)
-	}
-	return ts
 }
