e185b9eb5cd1d60e8c192b2b5f8321d0b31459dd
mircea-c <mircea@tendermint.com>
2019-07-16 08:22:32 -0400

    updated gaia install script

--- ami-gaia-sim/install_gaiad.sh
+++ ami-gaia-sim/install_gaiad.sh
@@ -2,7 +2,6 @@
 
 sudo mv /tmp/set_env.sh /etc/profile.d/set_env.sh
 sudo mv /tmp/genesis.json /home/ec2-user/genesis.json
-sudo chmod u+x /etc/profile.d/set_env.sh
 source /etc/profile.d/set_env.sh
 
 mkdir -p ${HOME}/go/bin
@@ -11,11 +10,7 @@ mkdir -p ${GOPATH}/src/github.com/cosmos
 cd ${GOPATH}/src/github.com/cosmos
 git clone https://github.com/cosmos/cosmos-sdk
 cd cosmos-sdk && git checkout ${GAIA_COMMIT_HASH}
-make tools
+make build
 
-cd ${GOPATH}/src/github.com/cosmos
-git clone https://github.com/cosmos/tools
-cd tools && git checkout mircea/runsim-upgrades
-go install ./cmd/runsim
-
+go install github.com/cosmos/tools/cmd/runsim
 sudo mv /home/ec2-user/go/bin/runsim /usr/bin/runsim
