3a507c1ba44999bbaeb79713795b53cced843879
mircea-c <mircea@tendermint.com>
2019-07-22 17:44:52 -0400

    Upgrading versions (#25)
    * changed hashi-go image to debian
    
    * updated hub node images
    
    * upgrading full nodes to latest version
    
    * upgrading full nodes to latest version
    
    * new path for gaia sim app
    
    * fixing Circle config
    
    * adding dev branch build
    
    * removed mod-readonly from go test
    
    * changed shutdown behavior
    
    * splitting up dev and prod sim build
    
    * changed instance shutdown behaviour
    
    * refactored startsim
    
    * Update seeds and shutdown behavior for prod
    
    * Reduce number of seeds because of AWS limits
    
    * Update # of seeds
    
    * upgrading full nodes to latest version
    
    * new path for gaia sim app
    
    * fixing Circle config
    
    * adding dev branch build
    
    * removed mod-readonly from go test
    
    * changed shutdown behavior
    
    * splitting up dev and prod sim build
    
    * changed instance shutdown behaviour
    
    * refactored startsim
    
    * added SQS message push
    
    * improved the program output
    
    * improved the program output
    
    * upgrading full nodes to latest version
    
    * refactored startsim
    
    * adding genesis to sim
    
    * fixed bug when sending SQS message
    
    * added packer build back in
    
    * changed the log object key
    
    * cleaning up startsim and fix index out of range bug
    
    * minor nits
    
    * 0.36 genesis file
    
    * oops, wrong 0.36 genesis
    
    * updated 0.36 genesis file
    
    * removed go code and updated circle config
    
    * updated gaia install script
    
    * changed how runsim is built. new genesis file
    
    * added npm to docs deploy image
    
    * added yarn to website deploy image
    
    * added make to build image
    
    * added npm to website build image
    
    * renamed website build image. not just yarn anymore
    
    * updated circleci config
    
    * removed useless image. updated go and terraform versions

---
+++ ami-gaia-sim/install_gaiad.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+sudo mv /tmp/set_env.sh /etc/profile.d/set_env.sh
+sudo mv /tmp/genesis.json /home/ec2-user/genesis.json
+source /etc/profile.d/set_env.sh
+
+mkdir -p ${HOME}/go/bin
+mkdir -p ${GOPATH}/src/github.com/cosmos
+
+cd ${GOPATH}/src/github.com/cosmos
+git clone https://github.com/cosmos/cosmos-sdk
+cd cosmos-sdk && git checkout ${GAIA_COMMIT_HASH}
+make build
+
+cd ${GOPATH}/src/github.com/cosmos
+git clone https://github.com/cosmos/tools
+cd tools && git checkout ${RUNSIM_COMMIT_HASH}
+go install ./cmd/runsim
+
+sudo mv /home/ec2-user/go/bin/runsim /usr/bin/runsim
--- docker-docs-deployment/README.md
+++
@@ -1,9 +0,0 @@
-### Docs deployment
-
-To pull this image:  
-`docker pull tendermintdev/docker-docs-deploy`  
-
-Image is used to trigger the automatic documentation build in the cosmos-sdk repo.
-
-Currently being used in the CircleCI config for the following repos:
-- https://github.com/cosmos/cosmos-sdk
