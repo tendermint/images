58696bc88f4cc7b8badd66b49a2597527e35c0eb
mircea-c <mircea@tendermint.com>
2019-07-22 10:28:38 -0400

    Added yarn to docker image (#21)
    * cors server, gaiad and gaiacli images
    
    * cors server configuration complete
    
    * changes to sim image, updated hub nodes config
    
    * added make to hashi-go image
    
    * adding zip to hasi-go image
    
    * changed hashi-go image to debian
    
    * updated hub node images
    
    * upgrading full nodes to latest version
    
    * upgrading full nodes to latest version
    
    * new path for gaia sim app
    
    * fixing Circle config
    
    * adding dev branch build
    
    * removed mod-readonly from go test
    
    * changed shutdown behavior
    
    * splitting up dev and prod sim build
    
    * changed instance shutdown behaviour
    
    * refactored startsim
    
    * Update seeds and shutdown behavior for prod
    
    * Reduce number of seeds because of AWS limits
    
    * Update # of seeds
    
    * upgrading full nodes to latest version
    
    * new path for gaia sim app
    
    * fixing Circle config
    
    * adding dev branch build
    
    * removed mod-readonly from go test
    
    * changed shutdown behavior
    
    * splitting up dev and prod sim build
    
    * changed instance shutdown behaviour
    
    * refactored startsim
    
    * added SQS message push
    
    * improved the program output
    
    * improved the program output
    
    * upgrading full nodes to latest version
    
    * refactored startsim
    
    * adding genesis to sim
    
    * fixed bug when sending SQS message
    
    * added packer build back in
    
    * changed the log object key
    
    * cleaning up startsim and fix index out of range bug
    
    * minor nits
    
    * 0.36 genesis file
    
    * oops, wrong 0.36 genesis
    
    * updated 0.36 genesis file
    
    * removed go code and updated circle config
    
    * updated gaia install script
    
    * changed how runsim is built. new genesis file
    
    * added npm to docs deploy image
    
    * added yarn to website deploy image

--- ami-gaia-sim/files/set_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH=$PATH:/usr/local/go/bin
-export HOME=/home/ec2-user
-export GOPATH=${HOME}/go
-export GOBIN=${GOPATH}/bin
-export PATH=$PATH:${GOBIN}
-export GO111MODULE=on
---
+++ ami-gaiad/mount_ebs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+mkdir -p ${HOME}/.gaiad
+sudo mount /dev/xvdx ${HOME}/.gaiad
